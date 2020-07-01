861306aa05d8a6bb14c88ee09b746690a6b450ce
Brian Remer <beremer@email.sc.edu>
2020-07-01 13:56:21 -0400

    Adding Xcode Project to GitHub

--- HeartRateApp WatchKit Extension/ExerciseInterfaceController.swift
+++ HeartRateApp WatchKit Extension/ExerciseInterfaceController.swift
@@ -2,8 +2,9 @@
 //  ExerciseInterfaceController.swift
 //  HeartRateApp WatchKit Extension
 //
-//  Created by Monica Remer on 6/3/20.
+//  Created by Brian Remer on 6/3/20.
 //  Copyright © 2020 Brian Remer. All rights reserved.
+//
 //
 
 import WatchKit
